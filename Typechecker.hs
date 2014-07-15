0b07ec8eaba16c82fb1acdbdf387d5bc056d3760
Benjamin Kovach <bkovach13@gmail.com>
2014-07-15 16:57:15 -0400

    throwError . TypeError -> typeError

--- Typechecker.hs
+++ Typechecker.hs
@@ -35,16 +35,19 @@ withCrumbAndScopedVar :: Typechecker a -> MoleculeCrumb -> Name -> Typechecker a
 withCrumbAndScopedVar action crumb var =
   local ((_1 .~ Just crumb) . (_2 %~ S.insert var)) action
 
+typeError :: String -> Typechecker a
+typeError = throwError . TypeError
+
 check :: MoleculeExpr -> Typechecker MoleculeType
 check (EVar name) = do
   (crumb, scope) <- ask
   if S.notMember name scope
-  then throwError . TypeError $ "variable not in scope: " ++ name
+  then typeError $ "variable not in scope: " ++ name
   else do
     env <- get
     case M.lookup name env of
       Nothing -> case crumb of
-        Nothing -> throwError . TypeError $ "cannot unify type for top-level variable " ++ name
+        Nothing -> typeError $ "cannot unify type for top-level variable " ++ name
         Just x -> case x of
           CPlusA _ -> addBinding name TInt
           CPlusB _ -> addBinding name TInt
@@ -52,34 +55,34 @@ check (EVar name) = do
           COrB   _ -> addBinding name TBool
           CAbs nm -> case M.lookup nm env of
             Just t  -> addBinding name t
-            Nothing -> throwError . TypeError $ "cannot unify type for variable " ++ nm
-          CApp1 _ -> throwError . TypeError $ name ++ " is not a function"
+            Nothing -> typeError $ "cannot unify type for variable " ++ nm
+          CApp1 _ -> typeError $ name ++ " is not a function"
           CApp2 e -> do
             t <- check e
             case t of
               TLam v _ -> addBinding name v
-              _ -> throwError . TypeError $ "cannot apply non-function to " ++ name
+              _ -> typeError $ "cannot apply non-function to " ++ name
       Just t -> case crumb of
         Nothing -> addBinding name t
         Just cb -> case cb of
           CPlusA _ | t == TInt -> addBinding name TInt
-                   | otherwise -> throwError . TypeError $ "type error in the first argument of +"
+                   | otherwise -> typeError $ "type error in the first argument of +"
           CPlusB _ | t == TInt -> addBinding name TInt
-                   | otherwise -> throwError . TypeError $ "type error in the second argument of +"
+                   | otherwise -> typeError $ "type error in the second argument of +"
           COrA   _ | t == TBool -> addBinding name TBool
-                   | otherwise -> throwError . TypeError $ "type error in the first argument of |"
+                   | otherwise -> typeError $ "type error in the first argument of |"
           COrB   _ | t == TBool -> addBinding name TBool
-                   | otherwise -> throwError . TypeError $ "type error in the second argument of |"
+                   | otherwise -> typeError $ "type error in the second argument of |"
           CAbs _ -> addBinding name t
           CApp1 _ -> case t of
             TLam _ _ -> addBinding name t
-            _        -> throwError . TypeError $ name ++ " is not a function"
+            _        -> typeError $ name ++ " is not a function"
           CApp2 e -> do
             t' <- check e
             case t' of
               TLam typ _ | typ == t -> addBinding name t
-                         | otherwise -> throwError . TypeError $ show e ++ " is not a function"
-              _ -> throwError . TypeError $ show e ++ " is not a function"
+                         | otherwise -> typeError $ show e ++ " is not a function"
+              _ -> typeError $ show e ++ " is not a function"
 
 check (e1 :+: e2) = do
   e1' <- check e1 `withCrumb` CPlusA e2
@@ -87,18 +90,18 @@ check (e1 :+: e2) = do
   bindings <- get
   case (e1', e2') of
     (TInt, TInt) -> return TInt
-    (_, TInt)    -> throwError . TypeError $ "type error in the first argument of +"
-    (TInt, _)    -> throwError . TypeError $ "type error in the second argument of +"
-    _            -> throwError . TypeError $ "type error in both arguments of +"
+    (_, TInt)    -> typeError $ "type error in the first argument of +, namely " ++ show e1 
+    (TInt, _)    -> typeError $ "type error in the second argument of +, namely " ++ show e2
+    _            -> typeError $ "type error in both arguments of +, namely " ++ show e1 ++ " and " ++ show e2
 
 check (e1 :|: e2) = do
   e1' <- check e1 `withCrumb` COrA e2
   e2' <- check e2 `withCrumb` COrB e1
   case (e1', e2') of
     (TBool, TBool) -> return TBool
-    (_, TBool)     -> throwError . TypeError $ "type error in the first argument of |"
-    (TBool, _)     -> throwError . TypeError $ "type error in the second argument of |"
-    _              -> throwError . TypeError $ "type error in both arguments of |"
+    (_, TBool)     -> typeError $ "type error in the first argument of |, namely " ++ show e1 
+    (TBool, _)     -> typeError $ "type error in the second argument of |, namely " ++ show e2
+    _              -> typeError $ "type error in both arguments of |, namely " ++ show e1 ++ " and " ++ show e2
 
 check (EApp e1 e2) = do
   t' <- check e2 `withCrumb` CApp2 e1
@@ -111,14 +114,14 @@ check (EApp e1 e2) = do
       t <- check e1 `withCrumb` CApp1 e1
       case t of 
         TLam t1 t2 | t1 == t'  -> return t2
-                   | otherwise -> throwError . TypeError $ "expecting " ++ show t1 ++ " but got " ++ show t' ++ " in function application"  
-        _ -> throwError . TypeError $ "expecting a function, but got " ++ show e1 ++ " in function application"
+                   | otherwise -> typeError $ "expecting " ++ show t1 ++ " but got " ++ show t' ++ " in function application"  
+        _ -> typeError $ "expecting a function, but got " ++ show e1 ++ " in function application"
 
 check (EAbs name expr) = do
   t   <- withCrumbAndScopedVar (check expr) (CAbs name) name
   env <- get
   case M.lookup name env of
-    Nothing -> throwError . TypeError $ "cannot infer type of " ++ name
+    Nothing -> typeError $ "cannot infer type of " ++ name
     Just t' -> return $ TLam t' t
 
 check ETrue    = return TBool
